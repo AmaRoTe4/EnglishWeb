49164a816870cb7e4210965a6c24b5e37215a9bb
Amaro <amaro7JJ@gmail.com>
2022-11-19 18:51:02 -0300

    agregando filtro

--- functions/AdivinaLaPalabra/juego/comprobar.ts
+++ functions/AdivinaLaPalabra/juego/comprobar.ts
@@ -4,7 +4,7 @@ export const resultado = (orden:boolean , respuesta:string , seleccionado:Palabr
     let respCorrecta:string[] = !orden ? seleccionado.palabraEnEspañol : seleccionado.palabraEnIngles;
 
     let aux:boolean[] = respCorrecta.map((n) => { 
-        return respuesta.toLowerCase() === n.toLowerCase() 
+        return respuesta.normalize('NFD').replace(/[\u0300-\u036f]/g,"").toLowerCase() === n.normalize('NFD').replace(/[\u0300-\u036f]/g,"").toLowerCase() 
     })
 
     return aux.includes(true)
